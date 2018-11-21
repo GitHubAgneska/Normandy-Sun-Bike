cd27dde71b4d55705b74c52a0cf55a14d68c48d5
Gaerdy <gaerdy@gmail.com>
2018-11-21 15:52:51 +0100

    Création composent log in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminLogInComponent } from './admin-log-in/admin-log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLogInComponent
   ],
   imports: [
     BrowserModule
