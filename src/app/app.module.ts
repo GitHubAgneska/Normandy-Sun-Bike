db74d0876cb4398b80a3ccdd46e216fe25af216e
etienne rabin <e.rabin05@gmail.com>
2018-11-23 09:20:40 +0100

    blog with responsivity 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RacesPresentationComponent } from './races-presentation/races-presentation.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BlogPageComponent } from './blog/blog-page/blog-page.component';
+import { BlogArticleElementComponent } from './blog/blog-article-element/blog-article-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RacesPresentationComponent
+    RacesPresentationComponent,
+    BlogPageComponent,
+    BlogArticleElementComponent
   ],
   imports: [
     BrowserModule,
