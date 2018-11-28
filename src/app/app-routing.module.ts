37b7ca131e9c82ccfd597ebb9bc2a8431f0a6238
etienne rabin <e.rabin05@gmail.com>
2018-11-28 14:29:10 +0100

    dev add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RacesPresentationComponent } from './races-presentation/races-presentation.component';
+import { BlogArticleElementComponent } from './blog/blog-article-element/blog-article-element.component';
 // importer le composent lier à la route
 
 const routes: Routes = [
   // Toujours déclaré sur ce model : {path: 'nom_du_chemin', component: nom_du_composant, pathMatch: 'full'}
-
+  {path: 'SunTrip', component: RacesPresentationComponent, pathMatch: 'full'},
+  {path: 'Blog', component: BlogArticleElementComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
