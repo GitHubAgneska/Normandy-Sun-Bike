00707f461a1c8c8ba56c174cc057c82384df8c8c
etienne rabin <e.rabin05@gmail.com>
2018-11-28 17:14:33 +0100

    project before presentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RacesPresentationComponent } from './races-presentation/races-presentation.component';
 import { BlogArticleElementComponent } from './blog/blog-article-element/blog-article-element.component';
+import { ProjectComponent } from './project/project.component';
 // importer le composent lier à la route
 
 const routes: Routes = [
   // Toujours déclaré sur ce model : {path: 'nom_du_chemin', component: nom_du_composant, pathMatch: 'full'}
   {path: 'SunTrip', component: RacesPresentationComponent, pathMatch: 'full'},
-  {path: 'Blog', component: BlogArticleElementComponent, pathMatch: 'full'}
+  {path: 'Blog', component: BlogArticleElementComponent, pathMatch: 'full'},
+  {path: 'Projet', component: ProjectComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
