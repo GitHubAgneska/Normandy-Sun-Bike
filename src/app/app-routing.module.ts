dab99d4113dd3cfc52dd63fb9289f40d4ff85908
Gaerdy <gaerdy@gmail.com>
2018-11-30 15:37:54 +0100

    Admin header template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RacesPresentationComponent } from './races-presentation/races-presentat
 import { BlogArticleElementComponent } from './blog/blog-article-element/blog-article-element.component';
 import { LogInComponent } from './admin/log-in/log-in.component';
 import { NavPageComponent } from './admin/nav-page/nav-page.component';
+// Provisoire pour test header page admin
+import { HeaderComponent } from './admin/header/header.component';
 // importer le composent lier à la route
 
 const routes: Routes = [
@@ -11,7 +13,11 @@ const routes: Routes = [
   {path: 'SunTrip', component: RacesPresentationComponent, pathMatch: 'full'},
   {path: 'Blog', component: BlogArticleElementComponent, pathMatch: 'full'},
   {path: 'admin', component: LogInComponent, pathMatch: 'full'},
-  {path: 'admin/navigation', component: NavPageComponent, pathMatch: 'full'}
+  {path: 'admin/navigation', component: NavPageComponent, pathMatch: 'full'},
+  // Provisoire pour test header page admin
+  {path: 'admin/header1', component: HeaderComponent, pathMatch: 'full', data: {title: 'ÉDITER SITE > Actualité'}},
+  {path: 'admin/header2', component: HeaderComponent, pathMatch: 'full', data: {title: 'ÉDITER SITE > Sponsors'}},
+  {path: 'admin/header3', component: HeaderComponent, pathMatch: 'full', data: {title: 'ÉDITER SITE > Courses'}}
 ];
 
 @NgModule({
