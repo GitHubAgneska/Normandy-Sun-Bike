7d4c6c3e27325ada618bcddc965aa125704711ab
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2018-12-05 18:39:50 +0100

    ajout sponsor image input

--- src/app/admin-sponsors/admin-sponsors.component.ts
+++ src/app/admin-sponsors/admin-sponsors.component.ts
@@ -10,6 +10,26 @@ export class AdminSponsorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+   /*  const fileInput = document.getElementById('file-input');
+    const output = document.getElementById('output');
+
+    function doSomethingWithFiles(fileList) {
+      let file = null;
+  
+      for (let i = 0; i < fileList.length; i++) {
+        if (fileList[i].type.match(/^image\//)) {
+          file = fileList[i];
+          break;
+        }
+      }
+  
+        if (file !== null) {
+          output.src = URL.createObjectURL(file);
+      }
+    }
+  
+      fileInput.addEventListener('change', (e) => doSomethingWithFiles(e.target.files)); */
   }
 
 }
