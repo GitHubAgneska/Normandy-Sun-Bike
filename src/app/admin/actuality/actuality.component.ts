cabd57991db2808c3e95ed4a0c58e68470be4205
Gaerdy <gaerdy@gmail.com>
2018-12-06 18:23:44 +0100

    Actuality page progress

--- src/app/admin/actuality/actuality.component.ts
+++ src/app/admin/actuality/actuality.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { calcPossibleSecurityContexts } from '@angular/compiler/src/template_parser/binding_parser';
 
 @Component({
   selector: 'app-actuality',
@@ -12,18 +11,41 @@ export class ActualityComponent implements OnInit {
                             { id: 'trip2019', name: '\"Sun Trip Tour 2019\"' },
                             { id: 'trip2020', name: '\"Sun Trip 2020\"' }];
 
-  public highlightedSection: number;
   public markup: String = '<img class="checkMark" src="assets/checkmark.png"/>';
-  public message: String = '';
+  public sectionNb: number = 0;
+  public sectionId = 'tripPlan';
+  public message: String;
 
-  confirmationMsg(): void {
-    this.message = 'La partie ' + this.sections[1].name + 'est désormais mise en avant !';
+  confirmationMsg(p_sectionNb): void {
+    this.message = 'La partie ' + this.sections[p_sectionNb].name + ' va être mise en avant !';
+  }
+
+  sectionSelected(p_id): void {
+    let sectionBlock: HTMLElement = document.getElementById(this.sectionId);
+
+    sectionBlock.classList.remove('selectedSection');
+
+    const sectionId = p_id;
+    const messagePart: HTMLElement = document.getElementById('confirmationMessage');
+
+    for (let i = 0; i < this.sections.length; i++) {
+      if (sectionId === this.sections[i].id) {
+        this.sectionNb = i;
+      }
+    }
+
+    this.confirmationMsg(this.sectionNb);
+
+    messagePart.setAttribute('style', 'opacity:1');
+
+    sectionBlock = document.getElementById(sectionId);
+    sectionBlock.classList.add('selectedSection');
   }
 
   constructor() { }
 
   ngOnInit() {
-
+    this.confirmationMsg(this.sectionNb);
   }
 
 }
