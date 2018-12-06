f8ff340b4098227634369f62cd34d76f643dde91
etienne rabin <e.rabin05@gmail.com>
2018-12-06 12:40:34 +0100

    sponsor initialized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { RacesPresentationComponent } from './races-presentation/races-presentation.component';
 import { BlogArticleElementComponent } from './blog/blog-article-element/blog-article-element.component';
 import { ProjectComponent } from './project/project.component';
+import { SponsorComponent } from './sponsor/sponsor.component';
 // importer le composent lier à la route
 
 const routes: Routes = [
   // Toujours déclaré sur ce model : {path: 'nom_du_chemin', component: nom_du_composant, pathMatch: 'full'}
   {path: 'SunTrip', component: RacesPresentationComponent, pathMatch: 'full'},
   {path: 'Blog', component: BlogArticleElementComponent, pathMatch: 'full'},
-  {path: 'Projet', component: ProjectComponent, pathMatch: 'full'}
+  {path: 'Projet', component: ProjectComponent, pathMatch: 'full'},
+  {path: 'Sponsors', component: SponsorComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BlogPageComponent } from './blog/blog-page/blog-page.component';
 import { BlogArticleElementComponent } from './blog/blog-article-element/blog-article-element.component';
 import { ProjectComponent } from './project/project.component';
+import { SponsorComponent } from './sponsor/sponsor.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ProjectComponent } from './project/project.component';
     BlogPageComponent,
     BlogArticleElementComponent,
     RacesPresentationComponent,
-    ProjectComponent
+    ProjectComponent,
+    SponsorComponent
   ],
   imports: [
     BrowserModule,
