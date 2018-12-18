f7057df3db9c0ce794fddd1e78431fcee28fcce2
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2018-12-18 17:29:50 +0100

    blog article class modif

--- src/app/admin/admin-blog-articles/BlogArticleElement_class.ts
+++ src/app/admin/admin-blog-articles/BlogArticleElement_class.ts
@@ -1,9 +1,16 @@
 
 
-export class BlogArticle{
-    id? :number;
-    title : string;
-    img : string;
-    date : string;
-    
+export class BlogArticle {
+    id:number;
+    title:string;
+    name:string;
+
+    year?:number;
+    month?:number;
+    day?:number;
+    fulldate? = function(){
+        return this.day + "/" + this.month +"/" + this.year;
+    }
+    img:string;
+    url?:string;
 }
