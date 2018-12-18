2c9c4b1ce5783aef4ad162265b50f12c76061660
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2018-12-18 15:39:51 +0100

    modify validate/cancel btns

--- src/app/admin/admin-sponsors/sponsorClass.ts
+++ src/app/admin/admin-sponsors/sponsorClass.ts
@@ -1,10 +1,14 @@
 
 
 export class Sponsor {
+<<<<<<< Updated upstream
     id? : number;
+=======
+    id: number;
+>>>>>>> Stashed changes
     name: string;
     img : string;
     link? : string;
-    level?: number;
+    level: number;
     description? : string;
 }
