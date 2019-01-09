5efecd2ef665e9a1368af453f747ec8b990615d8
etienne rabin <e.rabin05@gmail.com>
2019-01-09 09:38:35 +0100

    little change in controller

--- backNormandysunbike/src/main/java/com/normandysunbike/normandysunbike/controller/ActualityController.java
+++ backNormandysunbike/src/main/java/com/normandysunbike/normandysunbike/controller/ActualityController.java
@@ -1,10 +1,18 @@
 package com.normandysunbike.normandysunbike.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.normandysunbike.normandysunbike.entities.Actuality;
 import com.normandysunbike.normandysunbike.repository.ActualityRepository;
 
+@CrossOrigin( origins="*")
 @RestController
 public class ActualityController {
 
@@ -12,4 +20,18 @@ public class ActualityController {
 	ActualityRepository actualityRepo;
 	
 
+	@GetMapping( value="/actuality")
+    public List<Actuality> getAll(){
+		
+        return actualityRepo.findAll();
+        
+    }
+	
+	@PutMapping("/actuality")
+	public Actuality updateActuality() {
+		//@RequestBody Actuality p_question
+		
+		// Actuality actuality = actualityRepo.findById(id).get();
+		return null;
+	}
 }
--- backNormandysunbike/src/main/java/com/normandysunbike/normandysunbike/controller/SponsorController.java
+++ backNormandysunbike/src/main/java/com/normandysunbike/normandysunbike/controller/SponsorController.java
@@ -1,10 +1,12 @@
 package com.normandysunbike.normandysunbike.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.normandysunbike.normandysunbike.repository.SponsorRepository;
 
+@CrossOrigin( origins="*")
 @RestController
 public class SponsorController {
 	
