a55cf87a4c453e25d5cb9f16fe710d5d753752be
etienne rabin <e.rabin05@gmail.com>
2019-01-09 15:02:13 +0100

    sponsor mappings in controller

--- backNormandysunbike/src/main/java/com/normandysunbike/normandysunbike/controller/SponsorController.java
+++ backNormandysunbike/src/main/java/com/normandysunbike/normandysunbike/controller/SponsorController.java
@@ -1,9 +1,20 @@
 package com.normandysunbike.normandysunbike.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.normandysunbike.normandysunbike.entities.Sponsor;
 import com.normandysunbike.normandysunbike.repository.SponsorRepository;
 
 @CrossOrigin( origins="*")
@@ -13,5 +24,66 @@ public class SponsorController {
 	@Autowired
 	SponsorRepository SponsorRepo;
 
-
+	
+	@GetMapping("/sponsors")
+    public List<Sponsor> index(){
+        return SponsorRepo.findAll();
+    }
+	
+	@GetMapping("/sponsors/{id}")
+    public Sponsor show(@PathVariable int id){
+        return SponsorRepo.findById((long) id).get();
+    }
+	
+	@PostMapping("/sponsors")
+    public Sponsor create(@RequestBody Sponsor p_sponsor){
+        return SponsorRepo.save(p_sponsor);
+    }
+	
+    @PutMapping("/sponsors/{id}")
+    public Sponsor update(
+    		@PathVariable("id") Long p_id, 
+    		@RequestBody Sponsor p_sponsor
+    ) throws Exception 
+    {
+    	
+    	Sponsor current = SponsorRepo.findById(p_id).get();
+    	
+    	try {
+	    	if( p_sponsor.getName() != null ) {
+	    		current.setName(p_sponsor.getName());
+	    	}
+	    	
+	    	if( p_sponsor.getImg() != null ) {
+	    		current.setImg(p_sponsor.getImg());
+	    	}
+	    	
+	    	if( p_sponsor.getLink() != null ) {
+	    		current.setLink(p_sponsor.getLink());
+	    	}
+	    	
+	    	if( p_sponsor.getLevel() != null ) {
+	    		current.setLevel(p_sponsor.getLevel());
+	    	}
+	    	
+	    	if( p_sponsor.getDescription() != null ) {
+	    		current.setDescription(p_sponsor.getDescription());
+	    	}
+	    	
+	    	return SponsorRepo.save(current);
+    	}
+    	catch( Exception p_exception ) {
+    		throw new ResponseStatusException(
+	          HttpStatus.NOT_FOUND, 
+	          "no question found for id: " + p_id
+    		);
+    	}
+    }
+    
+    @DeleteMapping("blogs/{id}")
+    public boolean delete(@PathVariable int id){
+    	SponsorRepo.deleteById((long) id);
+        return true;
+    }
+	
 }
