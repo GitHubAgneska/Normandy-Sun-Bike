63c0059f9cf2c536dcd4128165187f2ec5f10600
etienne rabin <e.rabin05@gmail.com>
2019-01-11 11:20:54 +0100

    etienne install ng2 file upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule} from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
-<<<<<<< HEAD
 import { HttpModule } from '@angular/http';
-=======
 import { FileSelectDirective } from 'ng2-file-upload';
->>>>>>> origin/adminSponsors2
 
 
 
@@ -71,11 +68,8 @@ import { ArticleComponent } from './blog/article/article.component';
     AddBlogArticleComponent,
     EditSponsorComponent,
     EditBlogArticlesComponent,
-<<<<<<< HEAD
-    ArticleComponent
-=======
+    ArticleComponent,
     FileSelectDirective
->>>>>>> origin/adminSponsors2
   ],
   imports: [
     BrowserModule,
